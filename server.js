60ec8e931285e198b4303b5f52fa8ee0c33e2aff
Srihari Prasath <110505533+Srihari-Prasath@users.noreply.github.com>
2024-08-15 14:33:19 +0530

    front end | commit

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const taskRoutes = require('./routes/taskroutes');
-
-
-app.use('/api', taskRoutes);
 
 const app = express();
 const port = 3000;
+
+// Import routes after initializing the app
+const taskRoutes = require('./routes/taskRoutes');
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-//test
 mongoose.connect('mongodb://localhost/task-manager', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+// Use routes
+app.use('/api', taskRoutes);
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
+});
+// Add this before defining your routes
+app.get('/', (req, res) => {
+    res.send('Welcome to the Task Manager API!');
 });
